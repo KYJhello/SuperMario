02bab34d9b24b1ceb5edbf38d3efd24aa24c0f34
인권 백 <baek626@gmail.com>
2023-05-25 18:15:56 +0900

    feat : 오브젝트 씬 변경 / 아이템 이동 인터페이스 생성 / 아이템 클래스, 버섯 클래스 생성, 피직스2D 설정 수정

---
+++ Assets/Scripts/Item/Mushroom.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushroom : Item
+{
+    private void Awake()
+    {
+        itemInfo = Items.mushroom;     
+    }
+
+    public override void Move()
+    {
+    }
+
+    public override void Turn()
+    {
+    }
+
+}
