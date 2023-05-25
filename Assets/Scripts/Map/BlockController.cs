264d0e10eb594e87c1cfabac59eedc6c59867b7d
인권 백 <baek626@gmail.com>
2023-05-25 20:42:45 +0900

    feat : 블럭 구현 시작

--- Assets/Scripts/Map/BlockController.cs
+++ Assets/Scripts/Map/BlockController.cs
@@ -2,15 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockManager : MonoBehaviour
+public class BlockController : MonoBehaviour
 {
 
     private void Awake()
     {
+
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("콜리션엔터 발동");
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("콜리션스테이 발동");
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("트리거 발동");
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("트리거 발동");
+
     }
 }
